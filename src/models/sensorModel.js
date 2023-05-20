56b1e3d3ff0d60a3a085fd418b54760f69e65588
root <root@yichengliao1003@gmail.com>
2023-05-20 21:51:49 +0800

    update linux-server

--- src/models/sensorModel.js
+++
@@ -1,9 +0,0 @@
-module.exports = (sequelize, DataTypes) => {
-    const Sensor = sequelize.define("sensor", {
-        type: {
-            type: DataTypes.STRING,
-            allowNull: false
-        }
-    })
-    return Sensor
-}
